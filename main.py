0e6d35d8155298c746919e874ccbc2af6cf7b952
Anasucp <anas.ccj420@gmail.com>
2024-09-12 13:10:54 +0500

    complet html file with post method in form and dispaly area

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-from flask import  Flaks,render_template,request,redirect,url_for
-from json_person import read_data,write_data
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
